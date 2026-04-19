f52a8c950650c20a230a1ed781ee494f2a86e5f8
yakiTreeseason <maki05263@gmail.com>
2026-04-19 21:39:58 +0900

    docs: JavaScript 繰り返し処理

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -56,3 +56,84 @@ switch (color){
         console.log("colorの値が正しくありません");//どれとも異なった場合
         break;
 }
+//+1増やす
+let num = 1;
+console.log(num);
+
+num += 1;//変数numに1を加える
+console.log(num);
+
+//while文　繰り返し処理
+let numb = 1;//変数の定義
+while(numb <= 10){//変数numbの値が10以下の時に処理を繰り返す　条件式
+    console.log(numb);
+    numb += 1;//変数の更新
+}
+//for文　繰り返し処理
+for (let numbe = 1; numbe <= 10; numbe ++){//変数の定義;　条件式;　変数の更新
+    console.log(numbe);
+}
+//応用
+for(let nub = 1;nub <= 6;nub ++){
+    if(nub %3 === 0){//3の倍数の時だけメッセージが表示される
+        console.log("3の倍数です");
+    }else{
+        console.log(nub);
+    }
+}
+
+//配列を定数に代入
+const fruits = ["apple","banana","orange"];
+console.log(fruits);//["apple","banana","orange"]
+console.log(fruits[1]);//banana
+fruits[1] = "grape";//要素の値の更新
+console.log(fruits[1])//grape
+for(let i = 0; i < 3; i ++){//iが0-2の間ループする
+    console.log(fruits[i]);//変数iを用いて要素を取得
+}
+for(let i = 0; i< fruits.length; i ++){//lengthに置き換え可能　数字にすると時には大変
+    console.log(fruits[i]);
+}
+console.log(fruits.length);//3
+
+//オブジェクト
+const items =[
+    {name:"手裏剣",price:300},
+    {name:"忍者刀",price:1200}
+];
+console.log(items[0]);//{name: '手裏剣', price: 300}
+console.log(items[1].name);//忍者刀
+
+//応用
+const animals = [
+    {name:"猫",age:5},
+    {name:"犬",age:6},
+    {name:"うさぎ",age:3},
+    {name:"鳥"},
+];
+for (let i = 0; i < animals.length; i ++){
+    console.log("-------");//-------
+    const animal = animals[i];//animal = animals
+    console.log(`この子は${animal.name}です`);//名前はanimal.nameです
+    if(animal.age === undefined){//ageがない場合
+        console.log("年齢は秘密です");
+    } else { //ageがある場合
+        console.log(`${animal.age}歳です`);//animal.age歳です
+    }
+}
+
+//応用2
+const cafe = {
+    name: "yakiカフェ",
+    businesshours: { //businesshours　プロパティがオブジェクトになる
+        open: "10:00(AM)",
+        close: "8:00(PM)",
+    },
+    menus:["コーヒー","紅茶","緑茶"]//オブジェクトの値に複数の配列も可能
+};
+console.log(`店名:${cafe.name}`);//店名:yakiカフェ
+console.log(`営業時間:${cafe.businesshours.open}から${cafe.businesshours.close}`);//営業時間:10:00(AM)から8:00(PM)
+console.log(`おすすめメニューはこちら`);
+for (let i = 0; i < cafe.menus.length; i ++){
+    console.log(cafe.menus[i]);//"コーヒー","紅茶","緑茶"
+}
