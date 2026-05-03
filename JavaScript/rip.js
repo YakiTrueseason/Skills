b56fc165fe58e3f9dcb0aa845a52a1c90ff8785b
yakiTreeseason <maki05263@gmail.com>
2026-05-03 13:18:54 +0900

    docs:javascript npmパッケージ追加

---
+++ JavaScript/rip.js
@@ -0,0 +1,8 @@
+// import script from "./script";//script.jsで定義されたクラスが使用可能
+import { cat1,cat2 } from "./script";
+
+console.log("---------");
+cat1.info();
+console.log("---------");
+cat2.info();
+// export default rip;//ripクラスを他のファイルでも使用できるようにする設定
